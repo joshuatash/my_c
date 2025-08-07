eebb3075f34d50cae4b3e0a289e9b55c82534529
djtash <okutajoshuatash256@gmail.com>
2025-08-07 16:19:41 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    int age;
+    float gpa;
+
+    
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);  
+
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+
+    printf("Enter your GPA: ");
+    scanf("%f", &gpa);
+
+    printf("\n--- User Info ---\n");
+    printf("Name: %s", name); 
+    printf("Age: %d\n", age);
+    printf("GPA: %.2f\n", gpa);
+
+    return 0;
+}
